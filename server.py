f497790c3c43602c0e579096ab6750cfee98c86d
siliconer <siliconexplorer1@Gmail.com>
2015-04-14 16:46:01 +0800

    ^^O^^^

--- server.py
+++ server.py
@@ -14,13 +14,31 @@ define("port",default=8888,help="run on th given port",type=int)
 
 class  IndexHandler(tornado.web.RequestHandler):
 	def get(self):
-	        lst = ["python","www.itdiffer.com","qiwsir@gmail.com"]
-	        self.render("index.html", info=lst)
-
+		print '/'
+	 	# lst = ["python","www.itdiffer.com","qiwsir@gmail.com"]
+		# self.render("index_beginning.html", info=lst)
+		self.render("bootstrap_intro.html")
+	def write_error(self, status_code, **kwargs):
+      		  self.write("IndexHandler darnit, user! You caused a %d error." % status_code)
+class  SearchHandler(tornado.web.RequestHandler):
+	def  post(self):
+		self.write("searching")
+		search_term = self.get_argument('searchterm')
+		self.write("searching")
+		self.write(search_term);
+		self.render('index.html')
+	def write_error(self, status_code, **kwargs):
+       		 self.write("SearchHandler darnit, user! You caused a %d error." % status_code)
+class WrappHandler(tornado.web.RequestHandler):
+	def post(self):
+		text = self.get_argument('text')
+		self.write(text)
 def main():
 	tornado.options.parse_command_line()
 	application = tornado.web.Application(
-		handlers=[(r'/',IndexHandler)],
+		handlers=[(r'/',IndexHandler),
+		(r'/search',SearchHandler),
+		(r'/wrap',WrappHandler)] ,
    	    	template_path=os.path.join(os.path.dirname(__file__),"template"),
   	    	static_path=os.path.join(os.path.dirname(__file__),"static"),	
   	    	debug = True
