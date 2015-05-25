7990edde93618a5ecfc64bbaf1faba1cac1c2d72
siliconer <siliconexplorer1@Gmail.com>
2015-05-25 09:16:50 +0800

    post

--- stargate_post.py
+++ stargate_post.py
@@ -3,11 +3,24 @@ import base64
 import requests
 import os
 import os.path	
+from commands import *
+
 from starbase import Connection 
 def  stargate():
 	c = Connection()
 	c = Connection(host='127.0.0.1', port=7060)
 	print	c.tables()
 #	request = requests.get(baseurl + "/" + tablename + "/schema")
-
+	count_cmd = '/usr/bin/hbase org.apache.hadoop.hbase.mapreduce.RowCounter SRR1514737  read'
+	text = getoutput(cmd)
+	print text
+	rowkey = 200000
+	print 'row key '
+	t = c.table('SRR1514737')
+	t.insert(rowkey,
+		{
+		'read':'ACGT
+		})
+	print 'insert finished '
+	
 stargate()
